b326ac5c61835538cba949f078d50fca7fea8526
rnahill <rei333lly@gmail.com>
2024-06-16 12:10:54 -0400

    created initial creature forms

--- initiative-tracker/src/pages/HomePage/index.jsx
+++ initiative-tracker/src/pages/HomePage/index.jsx
@@ -1,7 +1,29 @@
+import "./style.css";
+
 export default function HomePage() {
     return (
-    <div>
-        <h1>Hello</h1>
-    </div>
+        <div>
+            <h1>D&D Initative Tracker</h1>
+            <section id="add-creature" className="border">
+                <div class="row">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Creature Name" aria-label="Creature Name"></input>
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Initiative" aria-label="Initiative"></input>
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="HP" aria-label="HP"></input>
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="AC" aria-label="AC"></input>
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Quantity" aria-label="Quantity"></input>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-dark" id="add-btn">Add</button>
+            </section>
+        </div>
     )
 }
